3d309830903a5b1629a3b5fcf4f92d03fd2edca0
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2017-06-29 20:57:33 +1000

    Fix iris example

--- iris.py
+++ iris.py
@@ -7,7 +7,7 @@ flowers = pd.read_csv('data/iris.csv')
 g = sns.pairplot(data=flowers, y_vars=['petal_length', 'petal_width'],
                  x_vars=['sepal_length', 'sepal_width'],
                  hue='species', plot_kws={'linewidth': 0})
-legend = g.fig.legend
-legend.draggable = True
+legend = g.fig.legends[0]
+legend.draggable(True)
 plt.show(block=True)
 g.fig.savefig('iris-pairs.png', dpi=300)
